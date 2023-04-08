b268dec6b94320426bb37a80d741741e40183386
Kirubhalakshmi <130222648+Kirubhalakshmi@users.noreply.github.com>
2023-04-08 17:10:22 +0530

    Initial files push

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import NewsList from './Components/NewsList';
+
+
+function App() {
+  return (
+    <>
+    <h2>Breaking News</h2>
+    <NewsList />
+    </>
+  );
+}
+
+export default App;
